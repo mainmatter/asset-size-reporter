27ccb9f0b27667162f71acbba1a7f185f8d36726
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-11-21 16:40:21 +0100

    Add `globby` dependency and smoke test

---
+++ tests/globby-test.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const globby = require('globby');
+
+const FIXTURE_PATH = path.join(__dirname, 'fixtures');
+
+describe('globby', () => {
+  test('finds and ignores files correctly', () => {
+    let cwd = path.join(FIXTURE_PATH, 'default');
+    let patterns = [
+      'dist/**/*.js',
+      '!dist/ignored-*.js',
+    ];
+
+    return expect(globby(patterns, { cwd })).resolves.toEqual([
+      'dist/file-inside-dist.js',
+      'dist/foo/nested-file-inside-dist.js',
+    ]);
+  });
+});
