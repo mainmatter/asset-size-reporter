996e27b20700fae84d1ca87ebc3dcc65cca1f826
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-11-22 17:41:44 +0100

    create-size-map: Adjust variable names

--- src/create-size-map.js
+++ src/create-size-map.js
@@ -1,15 +1,15 @@
-const path = require('path');
+const { resolve } = require('path');
 const globby = require('globby');
 
 const pathSizes = require('./path-sizes');
 
 module.exports = async (patterns, { cwd, gzip, brotli }) => {
-  let actualPaths = await globby(patterns, { cwd });
+  let paths = await globby(patterns, { cwd });
 
   let result = {};
-  for (let _path of actualPaths) {
-    let resolvedPath = path.resolve(cwd, _path);
-    result[_path] = await pathSizes(resolvedPath, { gzip, brotli });
+  for (let path of paths) {
+    let resolvedPath = resolve(cwd, path);
+    result[path] = await pathSizes(resolvedPath, { gzip, brotli });
   }
 
   return result;
