50dd9111bebc059e10693ee2b1d75673b256ad88
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-11-22 15:55:12 +0100

    Add `sum-sizes` module

---
+++ src/sum-sizes.js
@@ -0,0 +1,5 @@
+module.exports = (a, b) => ({
+  raw: a.raw + b.raw,
+  gzip: (a.gzip === null && b.gzip === null) ? null : a.gzip + b.gzip,
+  brotli: (a.brotli === null && b.brotli === null) ? null : a.brotli + b.brotli,
+});
