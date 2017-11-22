d4aaa8a65f36862258f16430bf8a42a5060bd3ee
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-11-22 21:36:47 +0100

    report-combined-size-map: Extract formatLine() function

--- src/report-combined-size-map.js
+++ src/report-combined-size-map.js
@@ -22,60 +22,66 @@ module.exports = (combined, { console }) => {
       sumBefore = sumSizes(sumBefore, before);
     }
 
-    let output = formatPathPrefix(path, { deleted: after === undefined });
-    if (after === undefined) {
-      // file was deleted
-      output += `${prettyBytes(before.raw)}`;
-      if (before.gzip !== null) {
-        output += ` / gzip ${prettyBytes(before.gzip)}`;
-      }
-      output += ` (deleted file)`;
-
-      output = chalk.gray(output);
-
-    } else if (before === undefined) {
-      // file was added
-      output += prettyBytes(after.raw);
-      if (after.gzip !== null) {
-        output += ` / gzip ${prettyBytes(after.gzip)}`;
-      }
-      output += ` (new file)`;
-
-      output = chalk.blue(output);
-
-    } else {
-      // file was modified
-      output += prettyBytes(before.raw);
-      if (before.raw !== after.raw) {
-        output += ` -> ${prettyBytes(after.raw)} (${prettyBytes(after.raw - before.raw, { signed: true })})`;
-      }
-
-      if (before.gzip !== null && after.gzip !== null) {
-        let previousGzip = before.gzip !== null ? prettyBytes(before.gzip) : '?';
-        let currentGzip = after.gzip !== null ? prettyBytes(after.gzip) : '?';
-        output += ` / gzip ${previousGzip}`;
-        if (before.gzip !== after.gzip) {
-          output += ` -> ${currentGzip} (${prettyBytes(after.gzip - before.gzip, { signed: true })})`;
-        }
-      }
-
-      if (before.raw > after.raw) {
-        output = chalk.green(output);
-      } else if (before.raw < after.raw) {
-        output = chalk.red(output);
-      }
-    }
+    let output = formatLine(path, before, after);
 
     console.log(output);
   }
 
-  let output = `Total: ${prettyBytes(sumAfter.raw)}`;
-  if (sumAfter.gzip !== null) {
-    output += ` / gzip ${prettyBytes(sumAfter.gzip)}`;
-  }
+  let output = formatLine('Total', sumBefore, sumAfter);
   console.log();
   console.log(output);
 };
+
+function formatLine(path, before, after) {
+  let output = path === 'Total'
+    ? chalk`{bold Total}{dim :} `
+    : formatPathPrefix(path, { deleted: after === undefined });
+
+  if (after === undefined) {
+    // file was deleted
+    output += `${prettyBytes(before.raw)}`;
+    if (before.gzip !== null) {
+      output += ` / gzip ${prettyBytes(before.gzip)}`;
+    }
+    output += ` (deleted file)`;
+
+    output = chalk.gray(output);
+
+  } else if (before === undefined) {
+    // file was added
+    output += prettyBytes(after.raw);
+    if (after.gzip !== null) {
+      output += ` / gzip ${prettyBytes(after.gzip)}`;
+    }
+    output += ` (new file)`;
+
+    output = chalk.blue(output);
+
+  } else {
+    // file was modified
+    output += prettyBytes(before.raw);
+    if (before.raw !== after.raw) {
+      output += ` -> ${prettyBytes(after.raw)} (${prettyBytes(after.raw - before.raw, { signed: true })})`;
+    }
+
+    if (before.gzip !== null && after.gzip !== null) {
+      let previousGzip = before.gzip !== null ? prettyBytes(before.gzip) : '?';
+      let currentGzip = after.gzip !== null ? prettyBytes(after.gzip) : '?';
+      output += ` / gzip ${previousGzip}`;
+      if (before.gzip !== after.gzip) {
+        output += ` -> ${currentGzip} (${prettyBytes(after.gzip - before.gzip, { signed: true })})`;
+      }
+    }
+
+    if (before.raw > after.raw) {
+      output = chalk.green(output);
+    } else if (before.raw < after.raw) {
+      output = chalk.red(output);
+    }
+  }
+
+  return output;
+}
 
 function formatPathPrefix(path, { deleted } = {}) {
   let dir = dirname(path);
