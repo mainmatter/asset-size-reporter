0d76852224c584c7ecb97cef95b6c36f9f5f3cf1
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-11-22 18:38:11 +0100

    create-size-map: Disable brotli options in tests
    TravisCI has no brotli :-/

--- tests/create-size-map-test.js
+++ tests/create-size-map-test.js
@@ -26,12 +26,12 @@ test('`fingerprinted` fixture: CSS+JS files', async () => {
     '**/*.js',
   ];
 
-  let sizeMap = await createSizeMap(patterns, { gzip: true, brotli: true, cwd });
+  let sizeMap = await createSizeMap(patterns, { gzip: true, cwd });
 
   expect(sizeMap).toEqual({
-    'index-384db273a7f0263ce0862ad0f6c6e98a.js': { raw: 3966, gzip: 650, brotli: 610 },
-    'nested/module-7de664e1a5819d059bcda372a0b198d9.js': { raw: 1855, gzip: 377, brotli: 357 },
-    'styles-807f81099a9ab6ff33cfede0fee287cc.css': { raw: 1846, gzip: 370, brotli: 348 }
+    'index-384db273a7f0263ce0862ad0f6c6e98a.js': { raw: 3966, gzip: 650, brotli: null },
+    'nested/module-7de664e1a5819d059bcda372a0b198d9.js': { raw: 1855, gzip: 377, brotli: null },
+    'styles-807f81099a9ab6ff33cfede0fee287cc.css': { raw: 1846, gzip: 370, brotli: null }
   });
 });
 
@@ -45,11 +45,11 @@ test('`fingerprinted` fixture: CSS+JS files with `fingerprintPattern`', async ()
 
   let fingerprintPattern = '(-[a-f\\d]{32})\\.';
 
-  let sizeMap = await createSizeMap(patterns, { gzip: true, brotli: true, fingerprintPattern, cwd });
+  let sizeMap = await createSizeMap(patterns, { gzip: true, fingerprintPattern, cwd });
 
   expect(sizeMap).toEqual({
-    'index.js': { raw: 3966, gzip: 650, brotli: 610 },
-    'nested/module.js': { raw: 1855, gzip: 377, brotli: 357 },
-    'styles.css': { raw: 1846, gzip: 370, brotli: 348 }
+    'index.js': { raw: 3966, gzip: 650, brotli: null },
+    'nested/module.js': { raw: 1855, gzip: 377, brotli: null },
+    'styles.css': { raw: 1846, gzip: 370, brotli: null }
   });
 });
