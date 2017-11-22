1189e2ea3e4659cb86ed3e6f7aadf0d0e4171660
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-11-22 17:25:28 +0100

    Extract `report-compared-sizes` module

--- src/asset-size-reporter.js
+++ src/asset-size-reporter.js
@@ -5,6 +5,7 @@ const prettyBytes = require('./pretty-bytes');
 const pathSizes = require('./path-sizes');
 const reportSizes = require('./report-sizes');
 const combineSizes = require('./combine-sizes');
+const reportComparedSizes = require('./report-compared-sizes');
 
 module.exports = async ({ patterns, json, compare, gzip, console, cwd }) => {
   if (gzip === undefined) {
@@ -25,46 +26,8 @@ module.exports = async ({ patterns, json, compare, gzip, console, cwd }) => {
 
   } else if (compare) {
     let combined = combineSizes(compare, result);
+    reportComparedSizes(combined, { console });
 
-    for (let _path of Object.keys(combined)) {
-      let sizes = combined[_path].after;
-      let previous = combined[_path].before;
-
-      let output = `${_path}: `;
-      if (sizes === undefined) {
-        // file was deleted
-        output += prettyBytes(previous.raw);
-        if (previous.gzip !== null) {
-          output += ` / gzip ${prettyBytes(previous.gzip)}`;
-        }
-        output += ` (deleted file)`;
-
-      } else if (previous === undefined) {
-        // file was added
-        output += prettyBytes(sizes.raw);
-        if (sizes.gzip !== null) {
-          output += ` / gzip ${prettyBytes(sizes.gzip)}`;
-        }
-        output += ` (new file)`;
-
-      } else {
-        // file was modified
-        output += `${prettyBytes(previous.raw)} -> ${prettyBytes(sizes.raw)}`;
-        if (previous.gzip !== null || sizes.gzip !== null) {
-          let previousGzip = previous.gzip !== null ? prettyBytes(previous.gzip) : '?';
-          let currentGzip = sizes.gzip !== null ? prettyBytes(sizes.gzip) : '?';
-          output += ` / gzip ${previousGzip} -> ${currentGzip}`;
-        }
-
-        output += ` (${prettyBytes(sizes.raw - previous.raw, { signed: true })}`;
-        if (previous.gzip !== null && sizes.gzip !== null) {
-          output += ` / gzip ${prettyBytes(sizes.gzip - previous.gzip, { signed: true })}`;
-        }
-        output += `)`;
-      }
-
-      console.log(output);
-    }
   } else {
     reportSizes(result, { console });
   }
---
+++ src/report-compared-sizes.js
@@ -0,0 +1,43 @@
+const prettyBytes = require('./pretty-bytes');
+
+module.exports = (combined, { console }) => {
+  for (let _path of Object.keys(combined)) {
+    let after = combined[_path].after;
+    let before = combined[_path].before;
+
+    let output = `${_path}: `;
+    if (after === undefined) {
+      // file was deleted
+      output += prettyBytes(before.raw);
+      if (before.gzip !== null) {
+        output += ` / gzip ${prettyBytes(before.gzip)}`;
+      }
+      output += ` (deleted file)`;
+
+    } else if (before === undefined) {
+      // file was added
+      output += prettyBytes(after.raw);
+      if (after.gzip !== null) {
+        output += ` / gzip ${prettyBytes(after.gzip)}`;
+      }
+      output += ` (new file)`;
+
+    } else {
+      // file was modified
+      output += `${prettyBytes(before.raw)} -> ${prettyBytes(after.raw)}`;
+      if (before.gzip !== null || after.gzip !== null) {
+        let previousGzip = before.gzip !== null ? prettyBytes(before.gzip) : '?';
+        let currentGzip = after.gzip !== null ? prettyBytes(after.gzip) : '?';
+        output += ` / gzip ${previousGzip} -> ${currentGzip}`;
+      }
+
+      output += ` (${prettyBytes(after.raw - before.raw, {signed: true})}`;
+      if (before.gzip !== null && after.gzip !== null) {
+        output += ` / gzip ${prettyBytes(after.gzip - before.gzip, {signed: true})}`;
+      }
+      output += `)`;
+    }
+
+    console.log(output);
+  }
+};
