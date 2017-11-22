534a7466d43c94d5e8614084739d14b2f87ad705
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-11-22 17:07:44 +0100

    Add `pretty-bytes` module

---
+++ src/pretty-bytes.js
@@ -0,0 +1,15 @@
+const prettyBytes = require('pretty-bytes');
+
+module.exports = (num, { signed } = {}) => {
+  if (signed) {
+    if (num === 0) {
+      return '±0 B';
+    }
+
+    let pretty = prettyBytes(num);
+    return num < 0 ? pretty : `+${pretty}`;
+
+  } else {
+    return prettyBytes(num);
+  }
+};
