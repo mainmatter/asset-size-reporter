761fc739f0dea84e8d49bfb266262b23bbc08ccd
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-11-22 20:23:05 +0100

    Add basic CLI script

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,5 @@
+'use strict';
+
 module.exports = {
   testMatch: [
     '<rootDir>/tests/**/*-test.js',
--- jest.lint.config.js
+++ jest.lint.config.js
@@ -4,6 +4,7 @@ module.exports = {
   displayName: 'lint',
   runner: 'jest-runner-eslint',
   testMatch: [
+    '<rootDir>/*.js',
     '<rootDir>/src/**/*.js',
     '<rootDir>/tests/**/*-test.js',
   ],
